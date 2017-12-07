7084f20b26a6a44a9e9dd01d1e84b72e2eed86f1
Andrew Eskenazi <drewesk@gmail.com>
2017-12-06 17:39:44 -0700

    Updated Screen Navigation

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StackNavigator } from 'react-navigation';
 import Profile from './components/Profile';
 import SplashPage from './components/SplashPage';
 import WebMap from './components/WebMap';
+import WaterUsage from './components/WaterUsage';
 
 const App = StackNavigator({
   Home: { screen: SplashPage },
   Profile: { screen: Profile },
-  WebMap: { screen: WebMap }
+  WebMap: { screen: WebMap },
+  WaterUsage: { screen: WaterUsage }
 });
 
 export default App;
